6c53babc4b21b4ecf4baf225074f82aa17ff51b1
Azat Abrarov <ufasat@yandex.ru>
2021-08-13 01:05:52 +0500

    fix card owner id

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,7 @@ import {CurrentUserContext} from '../contexts/CurrentUserContext'
 function Card ({card, onCardClick, onCardLike, onCardDelete}) {
   const currentUser = React.useContext(CurrentUserContext)
   
-  const isOwn = card.owner._id === currentUser._id
+  const isOwn = card.owner === currentUser._id
   const cardDeleteButtonClassName = (
     `button ${isOwn ? 'cards__delete-button' : 'cards__delete-button_hidden'}`
   )
